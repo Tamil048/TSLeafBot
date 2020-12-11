84a0b16b20b0151a15194f1d7b613e4c3ba3ced4
Tamil048 <tamilselvan.ark@gmail.com>
2020-12-11 21:16:07 +0530

    First commint in git

---
+++ src/main/java/com/qa/Utils/TestUtils.java
@@ -0,0 +1,7 @@
+package com.qa.Utils;
+
+public class TestUtils {
+	public void testUtils() {
+		System.out.println("TestUtils");
+	}
+}
---
+++ src/main/java/com/qa/libs/Test.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class Test {
+
+	public void method() {
+		System.out.println("Test Admin");
+	}
+}
